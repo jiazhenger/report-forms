242b45390dcd09421bf8faf9878b74ebed20b908
jiasiyan2923 <62272631+jiasiyan2923@users.noreply.github.com>
2020-06-21 18:24:49 +0800

    0

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 const Koa = require('koa')
 
 const app = new Koa()
-
+const port = 3005
 
 app.use( async ( ctx ) => {
-	ctx.body = 'hello koa2'
+	ctx.body = 'hello koa21232'
 })
 
-app.listen(3005)
+app.listen(port, ()=>{
+	console.log('localhost:',3005)
+})
